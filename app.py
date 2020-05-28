d9f065c802ecb78d916ceb3ead22119a02797232
avinashbatchala <61218554+avinashbatchala@users.noreply.github.com>
2020-05-27 23:30:41 -0400

    Added app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+from gtts import gTTS
+
+import os
+tts = gTTS(text='If you use Microsoft Windows 10, it has a speech engine included. Install the module win32com, then you can use this code:', lang='en')
+tts.save("mp3Files/hw.mp3")
+os.system("mpg321 good.mp3")

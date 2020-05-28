9e011e592ff1fca89741037405eaf6e0917ab9a3
avinashbatchala <61218554+avinashbatchala@users.noreply.github.com>
2020-05-27 23:34:53 -0400

    renamed mp3 sample file

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from gtts import gTTS
 
 import os
 tts = gTTS(text='If you use Microsoft Windows 10, it has a speech engine included. Install the module win32com, then you can use this code:', lang='en')
-tts.save("mp3Files/hw.mp3")
+tts.save("mp3Files/sample.mp3")
 os.system("mpg321 good.mp3")
